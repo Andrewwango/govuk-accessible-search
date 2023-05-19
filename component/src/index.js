6a8705e6b7f994423879d7bba4b77ebffec5483f
Andrew Wang <andrew.wang27@gmail.com>
2023-05-19 14:09:18 +0100

    remove typescript and revert to javascript

--- component/src/index.js
+++ component/src/index.js
@@ -1,11 +1,11 @@
 function searchButtonPressed() {
 
-    let inputElement = document.getElementById("user-query") as HTMLInputElement;
-    let outputElement = document.getElementById("search-result") as HTMLElement
+    let inputElement = document.getElementById("user-query")
+    let outputElement = document.getElementById("search-result")
     outputElement.innerHTML = handleSearch(inputElement.value)
 }
 
-function handleSearch(query: string): string {
+function handleSearch(query) {
     let documentText = scrapeCurrentPage()
     let prompt = constructPrompt(documentText, query)
     let response = callBackend(prompt) //may need to be asynchronous
@@ -13,16 +13,16 @@ function handleSearch(query: string): string {
     return response
 }
 
-function scrapeCurrentPage(): string {
+function scrapeCurrentPage() {
     // Scrape
     // Parse
     return ""
 }
 
-function constructPrompt(context: string, query: string) {
+function constructPrompt(context, query) {
     return query
 }
 
-function callBackend(prompt: string): string {
+function callBackend(prompt) {
     return "Your query was: " + prompt
 }
