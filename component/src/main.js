c622fabcb5c92a1e4147699f40bf2de895aeb348
Andrew Wang <andrew.wang27@gmail.com>
2023-05-22 11:37:09 +0100

    code to scrape and parse html

--- component/src/main.js
+++ component/src/main.js
@@ -1,7 +1,4 @@
-// Test webpack bundling by installing an example library such as 
-// geolib (`yarn add geolib`), importing it and using it in a function
-// (see callBackend). Webpack will copy geolib code into the dist js file.
-//const geolib = require('geolib');
+const htmlToText = require('html-to-text');
 
 document.getElementById("search-button").addEventListener("click", (event) => {
     let inputElement = document.getElementById("user-query")
@@ -9,34 +6,39 @@ document.getElementById("search-button").addEventListener("click", (event) => {
     outputElement.innerHTML = handleSearch(inputElement.value)
 });
 
-
-function searchButtonPressed() {
-
-    let inputElement = document.getElementById("user-query")
-    let outputElement = document.getElementById("search-result")
-    outputElement.innerHTML = handleSearch(inputElement.value)
-}
-
 function handleSearch(query) {
     let documentText = scrapeCurrentPage()
     let prompt = constructPrompt(documentText, query)
     let response = callBackend(prompt) //may need to be asynchronous
-
     return response
 }
 
 function scrapeCurrentPage() {
-    // Scrape
-    // Parse
-    return ""
+    const rawHtml = window.parent.document.body.outerHTML
+    const prettyText = htmlToText.convert(rawHtml, {
+        baseElements: {
+            selectors: ['div.govuk-govspeak']
+        },
+        selectors: [{
+            selector: 'a', 
+            options: {ignoreHref: true}
+        }]
+    })
+    return prettyText
 }
 
 function constructPrompt(context, query) {
-    return query
+    const prompt = 
+    `Given the following information:
+
+    ${context}
+
+    ${query}?
+    `
+    return prompt
 }
 
 function callBackend(prompt) {
-    // Uncomment to see webpack bundling in action
-    // geolib.convertDistance(3, "mm".toString())
-    return `Your query was: ${prompt}` 
+    console.log(prompt)
+    return `See console for prompt to be sent to backend` 
 }
