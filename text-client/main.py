8d2c1045ba9040cbb1a426d753dbd0bcda002463
Oliver Stanley <oliver.stanley@kainos.com>
2023-05-24 13:57:52 +0100

    Switch url

--- text-client/main.py
+++ text-client/main.py
@@ -7,7 +7,7 @@ app = typer.Typer()
 
 
 @app.command()
-def main(backend_url: str = "http://127.0.0.1:7071/api", action: str = "chatgpt"):
+def main(backend_url: str = "https://shwast-fun-app.azurewebsites.net/api", action: str = "chatgpt"):
     while True:
         try:
             message = typer.prompt("Query").strip()
