55741f600ae69057d2d346575d6e2ef895947807
Oliver Stanley <oliver.stanley@kainos.com>
2023-06-28 09:19:47 +0100

    STT auto-detect lang

--- backend/backend_function/services.py
+++ backend/backend_function/services.py
@@ -27,6 +27,10 @@ available_voices: list[speech.VoiceInfo] = speech.SpeechSynthesizer(
     speech_config=speech.SpeechConfig(subscription=AZURE_SPEECH_KEY, region=AZURE_SPEECH_REGION), audio_config=None
 ).get_voices_async().get().voices
 
+# For Azure at-start recognition we can only use 4. We could up to 10 using continuous recognition
+# This may become irrelevant if we use frontend STT (Webkit Speech API) instead of Azure
+speech_to_text_languages = ["en-GB", "de-DE", "es-ES", "fr-FR"]
+
 
 def perform_chat_completion(history: list[dict], prompt: str, parameters: dict, **kwargs) -> dict[str, str]:
     messages = history + [{"role": "user", "content": prompt}]
@@ -50,8 +54,11 @@ def perform_speech_to_text(filename: str) -> dict:
     speech_config = speech.SpeechConfig(subscription=AZURE_SPEECH_KEY, region=AZURE_SPEECH_REGION)
     speech_config.speech_recognition_language = "en-US"
     audio_config = speech.audio.AudioConfig(filename=filename)
+    detect_lang_config = speech.languageconfig.AutoDetectSourceLanguageConfig(languages=speech_to_text_languages)
 
-    recognizer = speech.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+    recognizer = speech.SpeechRecognizer(
+        speech_config=speech_config, audio_config=audio_config, auto_detect_source_language_config=detect_lang_config
+    )
 
     result = recognizer.recognize_once_async().get()
 
