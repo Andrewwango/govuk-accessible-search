33445748a299d476d9d94d834697d7422b649cfb
Oliver Stanley <oliver.stanley@kainos.com>
2023-06-29 09:19:40 +0100

    Add rate limiting to FastAPI backend

--- backend-fastapi/server.py
+++ backend-fastapi/server.py
@@ -1,3 +1,6 @@
+import asyncio
+from collections import defaultdict
+from datetime import datetime, timedelta
 from typing import Annotated
 
 import fastapi
@@ -6,6 +9,33 @@ import uvicorn
 from accessible_search import preprocessing, prompts, protocol, services
 
 app = fastapi.FastAPI()
+
+rate_limit_lock = asyncio.Lock()
+rate_limit_data = defaultdict(list)
+rate_limit_times = 10
+rate_limit_window = timedelta(seconds=60)
+
+
+@app.middleware("http")
+async def rate_limit_middleware(request: fastapi.Request, call_next):
+    """Rudimentary in-memory rate limiting solution."""
+    global rate_limit_data
+
+    client_ip = request.client.host
+    current_time = datetime.now()
+
+    async with rate_limit_lock:
+        rate_limit_data[client_ip] = [
+            time for time in rate_limit_data[client_ip]
+            if current_time - time < rate_limit_window
+        ]
+
+        if len(rate_limit_data[client_ip]) >= rate_limit_times:
+            raise fastapi.HTTPException(status_code=429, detail="Too Many Requests")
+
+        rate_limit_data[client_ip].append(current_time)
+
+    return await call_next(request)
 
 
 @app.post("/api/chatgpt", response_model=protocol.TextOutputResponse)
