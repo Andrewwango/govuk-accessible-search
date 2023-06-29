6724f1f80a539f455843a0394093818ddcba77ee
Oliver Stanley <oliver.stanley@kainos.com>
2023-06-29 11:10:18 +0100

    Prep container deploy

--- deploy-fastapi-container.sh
+++ deploy-fastapi-container.sh
@@ -1,1 +1,4 @@
-# TODO
+docker build . -f docker/Dockerfile.backend-fastapi -t shwastbackend
+az acr login --name shwastcontainers
+docker tag shwastbackend shwastcontainers.azurecr.io/shwastbackend
+docker push shwastcontainers.azurecr.io/shwastbackend
